53c3d429c32aa92e6ff946218cd301b07163807d
TorchAblaze <tiffanygreathead@gmail.com>
2021-08-15 00:03:51 -0700

    Add application state for guests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import GuestList from "./GuestList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    guests: [
+      {
+        name: "Treasure",
+        isConfirmed: false,
+      },
+      {
+        name: "Nic",
+        isConfirmed: true,
+      },
+      {
+        name: "Matt K",
+        isConfirmed: false,
+      },
+    ],
+  };
+
+  getTotalInvited = () => this.state.length;
+
+  getAttendingGuests = () => {
+    this.state.guests.forEach((guest) => {
+      let counter = 0;
+      if (guest["isConfirmed"]) {
+        counter += 1;
+      }
+      return counter;
+    });
+  };
+
+  getUnconfirmedGuests = () => {
+    return this.getTotalInvited - this.getAttendingGuests;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">
+              Submit
+            </button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList guests={this.state.guests} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
